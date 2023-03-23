2ada78b9681539d93ea4f88d3288bff93943ff62
CyberTea0X <120749144+CyberTea0X@users.noreply.github.com>
2023-03-23 16:41:19 +0300

    Попытка сделать авторизацию по jwt токенам

--- backend/actix_server/src/jwt.rs
+++ backend/actix_server/src/jwt.rs
@@ -1,18 +1,17 @@
-// use serde::Deserialize;
-// use crate::claims::Claims;
+use crate::claims::JwtConfig;
+use crate::claims::{create_jwt, Claims};
+use actix_web::Error;
+use serde::Deserialize;
 
-// #[derive(Deserialize)]
-// pub struct UserPermissions {
-//     pub username: String,
-//     pub permissions: Vec<String>,
-// }
+pub fn create_token(
+    email: &str,
+    permissions: Vec<String>,
+    jwt_config: &JwtConfig,
+) -> Result<String, Error> {
+    // Create a JWT
+    let claims = Claims::new(email.to_string(), permissions, jwt_config);
+    let jwt = create_jwt(claims, jwt_config)?;
 
-// pub fn create_token(info: web::Json<UserPermissions>) -> Result<String, Error> {
-//     let user_info = info.into_inner();
-//     // Create a JWT
-//     let claims = Claims::new(user_info.username, user_info.permissions);
-//     let jwt = claims::create_jwt(claims)?;
-
-//     // Return token for work with example handlers
-//     Ok(jwt)
-// }
+    // Return token for work with example handlers
+    Ok(jwt)
+}
