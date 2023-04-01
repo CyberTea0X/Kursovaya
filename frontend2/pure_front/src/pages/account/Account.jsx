299556803e4f8dd7c711c0edd35e4c33ce4d2cd4
lisichkadasha <122634421+lisichkadasha@users.noreply.github.com>
2023-04-01 22:34:01 +0300

    Add files via upload

--- frontend2/pure_front/src/pages/account/Account.jsx
+++ frontend2/pure_front/src/pages/account/Account.jsx
@@ -12,17 +12,15 @@ const Account = () => {
                         <img className="account-img" src={User} alt="" />
                         <button className="account-btn">Выбрать фотографию</button>
                     </div>
-                    <h3 className="account-title">Личная информация</h3>
-                    <p className="account-title">Моё имя*</p>
+                    <h3 className="account-title" style={{fontWeight: '700', padding:'0 0 20px 0'}}>Личная информация</h3>
+                    <p className="account-title">Имя пользователя</p>
                     <input className="account-input" placeholder="Введите имя" type="text" />
-                    <p className="account-title">Email</p>
-                    <input className="account-input" placeholder="Введите email" type="text" />
                     <p className="account-title">Обо мне</p>
-                    <input className="account-input2" placeholder="Напишите что-нибудь о себе" type="text" />
+                    <textarea style={{resize:'none'}} className="account-input2" placeholder="Напишите что-нибудь о себе" type="text" />
                     <p className="account-title">Введите номер телефона</p>
                     <input className="account-input" placeholder="Введите номер телефона" type="tel" />
                     <button className="account-btn2">Сохранить</button>
-                    <h3 className="account-title">Изменение пароля</h3>
+                    <h3 className="account-title" style={{fontWeight: '700', padding:'0 0 20px 0'}}>Изменение пароля</h3>
                     <p className="account-title">Текущий пароль</p>
                     <input className="account-input" placeholder="Текущий пароль" type="password"  />
                     <p className="account-title">Новый пароль</p>
