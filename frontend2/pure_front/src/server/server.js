fe30e946b94800f6b85740be0afc7f2bd83cf1ac
CyberTea0X <120749144+CyberTea0X@users.noreply.github.com>
2023-04-06 17:04:28 +0300

    Добавлена функция регистрации

---
+++ frontend2/pure_front/src/server/server.js
@@ -0,0 +1,23 @@
+import config from './config.json';
+
+
+async function registerUser(config, login, email, password, firstname, lastname, age, gender, about) {
+    const url = `http://${config.serverIp}:${config.serverPort}/api/user/register/${login}/${email}/${password}`;
+    const response = await fetch(url, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            login,
+            email,
+            password,
+            firstname,
+            lastname,
+            age,
+            gender,
+            about
+        })
+    });
+    return response.json();
+    }
