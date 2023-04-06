20b19f51c36f4ed3343761c591e9db5b86d6cf9a
CyberTea0X <120749144+CyberTea0X@users.noreply.github.com>
2023-04-06 19:17:15 +0300

    Пофикшен regex

--- backend/actix_server/src/email.rs
+++ backend/actix_server/src/email.rs
@@ -2,7 +2,7 @@ use regex::Regex;
 
 pub fn is_valid_email(email: &str) -> bool {
     let email_regex = Regex::new(
-        r"^([a-z0-9_+]([a-z0-9_+.]*[a-z0-9_+])?)@([a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,6})",
+        r"^([a-z0-9_+](\.?[a-z0-9_+])*@[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,6})$",
     )
     .unwrap();
     email_regex.is_match(email)
