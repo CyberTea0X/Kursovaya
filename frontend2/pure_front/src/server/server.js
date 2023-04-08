f540747f3aeeb89ce7bf2e130d93b6cbd57d7d96
CyberTea0X <120749144+CyberTea0X@users.noreply.github.com>
2023-04-09 00:27:47 +0300

    Добавлены функции login и userProfile

--- frontend2/pure_front/src/server/server.js
+++ frontend2/pure_front/src/server/server.js
@@ -1,8 +1,9 @@
 import config from '../config.json';
 
 
+const { "backend-ip": ip, "backend-port": port } = config;
+
 async function registerUser(login, email, password) {
-    const { "backend-ip": ip, "backend-port": port } = config;
     const url = `http://${ip}:${port}/api/user/register/${login}/${email}/${password}`;
     return new Promise((resolve, reject) => {
         fetch(url, {
@@ -17,4 +18,38 @@ async function registerUser(login, email, password) {
     });
 }
 
-export { registerUser };
+
+async function userProfile(idOrEmail) {
+    const query = (typeof idOrEmail === 'number') ? `id={idOrEmail}`: `email={idOrEmail}`;
+    const url = `http://${ip}:${port}/api/user/profile?${query}`;
+    return new Promise((resolve, reject) => {
+        fetch(url, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+        })
+        .then(response => response.json())
+        .then(data => resolve(data))
+        .catch(error => reject(error));
+    });
+}
+
+
+async function login(email, password) {
+    const url = `http://${ip}:${port}/api/user/login/${email}/${password}`;
+    return new Promise((resolve, reject) => {
+        fetch(url, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+        })
+        .then(response => response.json())
+        .then(data => resolve(data))
+        .catch(error => reject(error));
+    });
+}
+
+
+export { registerUser, login, userProfile };
