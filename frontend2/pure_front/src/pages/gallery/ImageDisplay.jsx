56bb968bf7276328a8c76c214956a65b47cd60ef
CyberTea0X <120749144+CyberTea0X@users.noreply.github.com>
2023-04-16 20:20:48 +0300

    Иконка карандаша

--- frontend2/pure_front/src/pages/gallery/ImageDisplay.jsx
+++ frontend2/pure_front/src/pages/gallery/ImageDisplay.jsx
@@ -1,7 +1,12 @@
+import { FaPencilAlt } from 'react-icons/fa';
 import './ImageDisplay.css';
-import React, {useEffect} from 'react';
+import React, { useEffect, useState } from 'react';
 
 const ImageDisplay = ({ img, onClose, onPrevious, onNext }) => {
+  const [isEditing, setIsEditing] = useState(false);
+  const [description, setDescription] = useState('Автор ничего не рассказал нам о картинке');
+  const [tags, setTags] = useState([]);
+
   const handlePrevious = () => {
     if (onPrevious) {
       onPrevious();
@@ -14,16 +19,34 @@ const ImageDisplay = ({ img, onClose, onPrevious, onNext }) => {
     }
   };
 
-  const handleKeyDown = (event) => {
-    if (event.key === 'Escape') {
-    onClose();
-    } else if (event.key === 'ArrowLeft' || event.keyCode === 65) {
-    handlePrevious();
-    } else if (event.key === 'ArrowRight' || event.keyCode === 68) {
-    handleNext();
-    }
+  const handleEdit = () => {
+    setIsEditing(true);
+  };
+
+  const handleSave = () => {
+    setIsEditing(false);
+  };
+
+  useEffect(() => {
+    const handleKeyDown = (event) => {
+      if (isEditing === false) {
+        if (event.key === 'Escape') {
+          onClose();
+        } else if (event.key === 'ArrowLeft' || event.keyCode === 65) {
+          handlePrevious();
+        } else if (event.key === 'ArrowRight' || event.keyCode === 68) {
+          handleNext();
+        }
+      }
     };
-    
+  
+    document.addEventListener('keydown', handleKeyDown);
+  
+    return () => {
+      document.removeEventListener('keydown', handleKeyDown);
+    };
+  }, [isEditing, onClose, handlePrevious, handleNext]);
+
   useEffect(() => {
     document.addEventListener('keydown', handleKeyDown);
     return () => {
@@ -36,12 +59,37 @@ const ImageDisplay = ({ img, onClose, onPrevious, onNext }) => {
       <button className="button-close" onClick={onClose}>
         X
       </button>
+      <div className="image-container">
+        <img src={img} className="image" />
+        <div style={{ color: 'white' }} onClick={handleEdit}>
+            <FaPencilAlt />
+        </div>
+        {isEditing && (
+            <div className="edit-container">
+            <textarea
+                className="description-input"
+                placeholder="Enter description"
+                value={description}
+                onChange={(e) => setDescription(e.target.value)}
+            />
+            <input
+                className="tags-input"
+                placeholder="Enter tags"
+                value={tags.join(',')}
+                onChange={(e) => setTags(e.target.value.split(','))}
+            />
+            <button className="save-button" onClick={handleSave}>
+                Save
+            </button>
+            </div>
+        )}
+        {!isEditing && (
+        <p className="description">{description}</p>
+        )}
+      </div>
       <button className="button-previous" onClick={handlePrevious}>
         Previous
       </button>
-      <div className="image-container">
-        <img src={img} className="image" />
-      </div>
       <button className="button-next" onClick={handleNext}>
         Next
       </button>
@@ -49,4 +97,4 @@ const ImageDisplay = ({ img, onClose, onPrevious, onNext }) => {
   );
 };
 
-export {ImageDisplay}
+export { ImageDisplay };
