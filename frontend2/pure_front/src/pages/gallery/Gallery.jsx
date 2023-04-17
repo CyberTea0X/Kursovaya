b0255a2866ffee819e2a446fefe65cf2a7953eb0
CyberTea0X <120749144+CyberTea0X@users.noreply.github.com>
2023-04-17 14:41:20 +0300

    Добавлена кнопка для загрузки картинок

--- frontend2/pure_front/src/pages/gallery/Gallery.jsx
+++ frontend2/pure_front/src/pages/gallery/Gallery.jsx
@@ -1,88 +1,113 @@
-import React, {useState} from 'react';
-import Masonry,{ResponsiveMasonry} from 'react-responsive-masonry';
-import "./gallery.css";
-import User from "../account/Unknown_person.jpg"
+import React, { useState, useEffect } from 'react';
+import Masonry, { ResponsiveMasonry } from 'react-responsive-masonry';
+import './gallery.css';
+import User from '../account/Unknown_person.jpg';
 import { ImageDisplay } from './ImageDisplay';
-const images = [
-"https://i.pinimg.com/474x/98/64/58/98645877a607cc968e459047a6eba601.jpg",
-"https://i.pinimg.com/564x/24/10/32/241032236c043b846d106cc36d26f5aa.jpg",
-"https://i.pinimg.com/474x/62/77/90/6277908fb99b73cff178cc590e29f79c.jpg",
-"https://i.pinimg.com/474x/90/7c/40/907c40a5d8c318f8cb1235677b73372b.jpg",
-"https://i.pinimg.com/474x/ab/f7/b4/abf7b45e7dd6e108872bb9ef505d4818.jpg",
-"https://i.pinimg.com/474x/97/33/12/973312f9abd98ccfb541890d3750ad6e.jpg",
-"https://i.pinimg.com/474x/4e/39/c7/4e39c7322805c2b1176e8917603af005.jpg",
-"https://i.pinimg.com/474x/12/e3/73/12e373a1e3b51aadad0d1153bf40bbf9.jpg",
-"https://i.pinimg.com/474x/73/52/ef/7352ef46912cc353637425fab96027c7.jpg",
-"https://i.pinimg.com/474x/e3/40/f7/e340f7457647c291d249d2abfa610c4e.jpg",
-"https://i.pinimg.com/474x/b4/b5/43/b4b5433a7722124be16ae5dd5cf410aa.jpg",
-"https://i.pinimg.com/474x/18/35/1f/18351f19449959523259c52efa5e15eb.jpg",
-"https://i.pinimg.com/474x/e9/f8/4b/e9f84b03ae41bed71a9047fd254e0e78.jpg",
-"https://i.pinimg.com/474x/38/d6/64/38d6640bdd5997bb49d035f21ccb5f6e.jpg",
-"https://i.pinimg.com/474x/24/fa/f7/24faf7e2d2d1195e879d3a86bf138955.jpg",
-]
+import { FaPlusSquare } from 'react-icons/fa';
+
 
 const Gallery = () => {
-    const [data, setData] = useState({ img: '', i: 0 });
-    const totalImages = images.length;
-  
-    // function to display an image
-    const viewImage = (img, i) => {
-      setData({ img, i });
-    };
-  
-    // function to handle image navigation
-    const imgAction = (action) => {
-      let i = data.i;
-      if (action === 'next-img') {
-        i = (i + 1) % totalImages;
-        setData({ img: images[i], i });
-      }
-      if (action === 'previous-img') {
-        i = (i - 1 + totalImages) % totalImages;
-        setData({ img: images[i], i });
-      }
-      if (!action) {
-        setData({ img: '', i: 0 });
-      }
-    };
-  
-    // render the gallery UI
-    return (
-      <>
-        {/* display the image button */}
-        {data.img ? (
-          <ImageDisplay
-            img={data.img}
-            onClose={() => imgAction()}
-            onPrevious={() => imgAction('previous-img')}
-            onNext={() => imgAction('next-img')}
-          />
-        ) : null}
-  
-        {/* display the user profile image */}
-        <img className="profile-img" src={User} alt="" />
-  
-        {/* display the image grid */}
-        <div style={{ padding: '10px' }}>
-          <ResponsiveMasonry
-            columnsCountBreakPoints={{ 350: 3, 750: 4, 900: 5 }}
-          >
-            <Masonry gutter='10px'>
-              {images.map((image, i) => (
-                <img
-                  key={i}
-                  src={image}
-                  style={{ width: '100%', display: 'block', cursor: 'pointer', borderRadius: '30px' }}
-                  alt=""
-                  onClick={() => viewImage(image, i)}
-                />
-              ))}
-            </Masonry>
-          </ResponsiveMasonry>
-        </div>
-      </>
-    );
-  };
-  
-  export { Gallery };
+  const [data, setData] = useState({ img: '', i: 0 });
+  const [uploadFormActive, setUploadFormActive] = useState(false);
+  const [images, setImages] = useState([]);
+  const totalImages = images.length;
 
+  // function to display an image
+  const viewImage = (img, i) => {
+    setData({ img, i });
+  };
+
+  // function to handle image navigation
+  const imgAction = (action) => {
+    let i = data.i;
+    if (action === 'next-img') {
+      i = (i + 1) % totalImages;
+      setData({ img: images[i], i });
+    }
+    if (action === 'previous-img') {
+      i = (i - 1 + totalImages) % totalImages;
+      setData({ img: images[i], i });
+    }
+    if (!action) {
+      setData({ img: '', i: 0 });
+    }
+  };
+
+  // function to handle image upload
+  const handleImageUpload = (e) => {
+    if (e.target.files.length == 0) {
+      return;
+    }
+    const file = e.target.files[0];
+    const reader = new FileReader();
+
+    reader.onload = () => {
+      const newImages = [...images, reader.result]; 
+      // создаем новый массив, содержащий все старые изображения
+      // и новое добавленное изображение
+      setImages(newImages); // устанавливаем новый массив в качестве состояния images
+    };
+
+    reader.readAsDataURL(file);
+  };
+
+  // render the gallery UI
+  return (
+    <>
+      {/* display the image button */}
+      {data.img ? (
+        <ImageDisplay
+          img={data.img}
+          onClose={() => imgAction()}
+          onPrevious={() => imgAction('previous-img')}
+          onNext={() => imgAction('next-img')}
+        />
+      ) : null}
+
+      {/* display the user profile image */}
+      <img className="profile-img" src={User} alt="" />
+
+      {/* display the image grid */}
+      <div style={{ padding: '10px' }}>
+        <ResponsiveMasonry
+          columnsCountBreakPoints={{ 350: 3, 750: 4, 900: 5 }}
+        >
+          <Masonry gutter="10px">
+            {images.map((image, i) => (
+              <img
+                key={i}
+                src={image}
+                style={{
+                  width: '100%',
+                  display: 'block',
+                  cursor: 'pointer',
+                  borderRadius: '30px',
+                }}
+                alt=""
+                onClick={() => viewImage(image, i)}
+              />
+            ))}
+          </Masonry>
+        </ResponsiveMasonry>
+      </div>
+
+      {/* display the image upload form */}
+      <div
+        className={`upload-form ${uploadFormActive ? 'active' : ''}`}
+        onClick={() => setUploadFormActive(!uploadFormActive)}
+      >
+        <label htmlFor="image-upload" className="upload-label">
+          <FaPlusSquare size={50} color="#ccc" />
+        </label>
+        <input
+          type="file"
+          id="image-upload"
+          accept="image/*"
+          onChange={handleImageUpload}
+        />
+      </div>
+    </>
+  );
+};
+  
+export { Gallery };
