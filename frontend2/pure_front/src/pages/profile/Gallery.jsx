69a4790f95bad29cd40ee8a19595aa5ca21a20a1
CyberTea0X <120749144+CyberTea0X@users.noreply.github.com>
2023-04-20 16:33:27 +0300

    Профиль разбит на галерею и профиль

---
+++ frontend2/pure_front/src/pages/profile/Gallery.jsx
@@ -0,0 +1,116 @@
+import React, { useState, useEffect } from 'react';
+import Masonry, { ResponsiveMasonry } from 'react-responsive-masonry';
+import { FaPlusSquare } from 'react-icons/fa';
+import { ImageView } from './ImageDisplay';
+import { upload_image } from '../../server/requests';
+import { getImages } from '../../server/requests_handler';
+import Cookies from 'js-cookie';
+import './profile.css';
+
+
+const Gallery = ({user, isOwner}) => {
+  const [data, setData] = useState({ img: '', i: 0 });
+  const [uploadFormActive, setUploadFormActive] = useState(false);
+  const [images, setImages] = useState([]); // хранение данных об изображениях
+  const totalImages = images.length;
+  // function to display an image
+  const viewImage = (img, i) => {
+    setData({ img, i });
+  };
+
+  const getGallery = async () => {
+    setImages(await getImages(user.id));
+  }
+
+  useEffect(() => {
+    if (user.id == undefined) {
+      return;
+    }
+    getGallery();
+  }, [user]);
+
+  // function to handle image navigation
+  const imgAction = (action) => {
+    let i = data.i;
+    if (action === 'next-img') {
+      i = (i + 1) % totalImages;
+      setData({ img: images[i], i });
+    }
+    if (action === 'previous-img') {
+      i = (i - 1 + totalImages) % totalImages;
+      setData({ img: images[i], i });
+    }
+    if (!action) {
+      setData({ img: '', i: 0 });
+    }
+  };
+
+  // function to handle image upload
+  const handleImageUpload = (e) => {
+    if (e.target.files.length == 0) {
+      return;
+    }
+    const file = e.target.files[0];
+    let email = Cookies.get("email").toLowerCase();
+    let pw = Cookies.get("password");
+    upload_image(email, pw, file).then(() => {getGallery()})
+  };
+
+  // render the gallery UI
+  return (
+    <>
+      {/* display the image button */}
+      {data.img ? (
+        <ImageView
+          img={data.img}
+          onClose={() => imgAction()}
+          onPrevious={() => imgAction('previous-img')}
+          onNext={() => imgAction('next-img')}
+          isOwner={isOwner}
+        />
+      ) : null}
+      <div style={{ padding: '10px'}}>
+        <ResponsiveMasonry
+          columnsCountBreakPoints={{ 350: 3, 750: 4, 900: 5 }}
+        >
+          <Masonry gutter="10px">
+            {images.map((image, i) => (
+              <img
+                key={i}
+                src={image.url}
+                style={{
+                  width: '100%',
+                  display: 'block',
+                  cursor: 'pointer',
+                  borderRadius: '30px',
+                }}
+                alt=""
+                onClick={() => viewImage(image, i)}
+              />
+            ))}
+          </Masonry>
+        </ResponsiveMasonry>
+      </div>
+
+      {/* display the image upload form */}
+      {isOwner && (
+        <div
+          className={`upload-form ${uploadFormActive ? 'active' : ''}`}
+          onClick={() => setUploadFormActive(!uploadFormActive)}
+        >
+          <label htmlFor="image-upload" className="upload-label">
+            <FaPlusSquare size={50} color="#ccc" />
+          </label>
+          <input
+            type="file"
+            id="image-upload"
+            accept="image/*"
+            onChange={handleImageUpload}
+          />
+        </div>
+      )}
+    </>
+  );
+};
+
+export { Gallery };
--- frontend2/pure_front/src/pages/profile/Profile.jsx
+++ frontend2/pure_front/src/pages/profile/Profile.jsx
@@ -1,34 +1,29 @@
 import React, { useState, useEffect } from 'react';
-import Masonry, { ResponsiveMasonry } from 'react-responsive-masonry';
 import './profile.css';
+import { FaCog } from 'react-icons/fa';
 import UnknownPerson from "../img/Unknown_person.jpg";
-import { ImageView } from './ImageDisplay';
-import { FaPlusSquare } from 'react-icons/fa';
 import { User } from '../../types';
 import { useNavigate } from 'react-router-dom';
 import Cookies from 'js-cookie';
-import { getUserProfile, getImages, getTagsArray, getAvatarImage } from '../../server/requests_handler';
-import { upload_image, visit } from '../../server/requests';
+import { getUserProfile, getTagsArray, getImages, getAvatarImage} from '../../server/requests_handler';
+import { visit } from '../../server/requests';
 import { useParams } from 'react-router-dom';
+import { Gallery } from './Gallery';
 
 
 const Profile = () => {
-  const [data, setData] = useState({ img: '', i: 0 });
-  const [uploadFormActive, setUploadFormActive] = useState(false);
-  const [images, setImages] = useState([]);
   const [user, setUser] = useState(User.emptyUser());
   const [avatar, setAvatar] = useState(UnknownPerson);
   const [isOwner, setIsOwner] = useState(false);
   const [tags, setTags] = useState([]); // хранение данных о тегах
   const { userId } = useParams();
-  const totalImages = images.length;
 
   let navigate = useNavigate(); 
   const routeChange = (route) =>{ 
       let path = `/${route}`; 
       navigate(path);
   }
-    
+
   const getAccount = async () => {
     let userIdOrEmail;
       try {
@@ -45,22 +40,6 @@ const Profile = () => {
       }
   }
 
-  const getGallery = async () => {
-    setImages(await getImages(user.id));
-  }
-
-  const loadAvatar = async () => {
-    let logo_img = await getAvatarImage(user.id);
-    if (logo_img !== undefined) {
-      setAvatar(logo_img.url);
-    }
-  }
-
-  // function to display an image
-  const viewImage = (img, i) => {
-    setData({ img, i });
-  };
-
   const retrieveUserTags = async () => {
     try {
         let tags_arr = await getTagsArray(user.id);
@@ -72,46 +51,23 @@ const Profile = () => {
         alert(error.message);
     }
   }
-  
 
-  // function to handle image navigation
-  const imgAction = (action) => {
-    let i = data.i;
-    if (action === 'next-img') {
-      i = (i + 1) % totalImages;
-      setData({ img: images[i], i });
+  const loadAvatar = async () => {
+    let logo_img = await getAvatarImage(user.id);
+    if (logo_img !== undefined) {
+      setAvatar(logo_img.url);
     }
-    if (action === 'previous-img') {
-      i = (i - 1 + totalImages) % totalImages;
-      setData({ img: images[i], i });
-    }
-    if (!action) {
-      setData({ img: '', i: 0 });
-    }
-  };
-
-  // function to handle image upload
-  const handleImageUpload = (e) => {
-    if (e.target.files.length == 0) {
-      return;
-    }
-    const file = e.target.files[0];
-    let email = Cookies.get("email").toLowerCase();
-    let pw = Cookies.get("password");
-    upload_image(email, pw, file).then(() => {getGallery()})
-  };
+  }
 
   useEffect(() => {
     getAccount();
   }, []);
+
   useEffect(() => {
     if (user.id == undefined) {
       return;
     }
     retrieveUserTags()
-    if (images.length == 0) {
-      getGallery();
-    }
     if (user.id == Cookies.get("id")) {
       setIsOwner(true)
     }
@@ -123,27 +79,9 @@ const Profile = () => {
     loadAvatar()
   }, [user]);
 
-
-  useEffect(() => {
-    if (user.id == undefined) {
-      return;
-    }
-    getGallery();
-  }, [data.img]);
-
-  // render the gallery UI
+  // render the profile UI
   return (
     <>
-      {/* display the image button */}
-      {data.img ? (
-        <ImageView
-          img={data.img}
-          onClose={() => imgAction()}
-          onPrevious={() => imgAction('previous-img')}
-          onNext={() => imgAction('next-img')}
-          isOwner={isOwner}
-        />
-      ) : null}
       <div className="user-info">
         <div className="main-info">
           <div className="user-name"> {user.username} </div>
@@ -170,52 +108,15 @@ const Profile = () => {
             ))}
           </div>
         </div>
+        <div className='profile-cog'>
+          <FaCog style = {{cursor: 'pointer'}} onClick={()=>routeChange("Settings")}></FaCog>
+        </div>
       </div>
 
       {/* display the image grid */}
-      <div style={{ padding: '10px'}}>
-        <ResponsiveMasonry
-          columnsCountBreakPoints={{ 350: 3, 750: 4, 900: 5 }}
-        >
-          <Masonry gutter="10px">
-            {images.map((image, i) => (
-              <img
-                key={i}
-                src={image.url}
-                style={{
-                  width: '100%',
-                  display: 'block',
-                  cursor: 'pointer',
-                  borderRadius: '30px',
-                }}
-                alt=""
-                onClick={() => viewImage(image, i)}
-              />
-            ))}
-          </Masonry>
-        </ResponsiveMasonry>
-      </div>
-
-      {/* display the image upload form */}
-        {isOwner && (
-          <div
-          className={`upload-form ${uploadFormActive ? 'active' : ''}`}
-          onClick={() => setUploadFormActive(!uploadFormActive)}
-        >
-                  <label htmlFor="image-upload" className="upload-label">
-            <FaPlusSquare size={50} color="#ccc" />
-          </label>
-          <input
-            type="file"
-            id="image-upload"
-            accept="image/*"
-            onChange={handleImageUpload}
-          />
-          </div>
-        )}
- 
+      <Gallery user={user} isOwner={isOwner}/>
     </>
   );
 };
-  
+
 export { Profile };
